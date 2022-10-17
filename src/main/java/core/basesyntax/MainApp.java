083e1cb1e8786d92c87f29c74155882ef490a09e
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-10-17 14:46:46 +0200

    task done

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
 public class MainApp {
+    public class Excavator extends Machine{
+        @Override
+        public void doWork() {
+            System.out.println("Excavator is working");
+
+        }
+        @Override
+        public void stopWork() {
+            System.out.println("Excavator stopped working");
+        }
+    }
+
 
 }
