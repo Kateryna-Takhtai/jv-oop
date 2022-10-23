e4fbc014f1e1f06c8857622645cb6bf5b1a6dc98
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-10-23 18:01:32 +0200

    task1 done

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -4,7 +4,6 @@ public class Excavator extends Machine{
     @Override
     public void doWork() {
         System.out.println("Excavator is working");
-
     }
     @Override
     public void stopWork() {
